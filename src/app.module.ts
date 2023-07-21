b255934adc72a6eaa3828b94e1929a0ee290a9c2
Felipe Kafuri <felipe11.rk@gmail.com>
2023-07-21 19:01:35 -0300

    update project folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { UsersModule } from './users/users.module'
+import { UsersModule } from './app/users/users.module'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
