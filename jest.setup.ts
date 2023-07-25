4cad833ac06ca14fb6b4a9ab33bc520823f62cb4
Felipe Kafuri <felipe11.rk@gmail.com>
2023-07-25 16:45:38 -0300

    .

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,7 +5,7 @@ beforeAll(async () => {
   const dbName = 'rga_dev_test' // replace with your database name
   try {
     // Try to create the database
-    await prisma.$queryRaw(Prisma.sql`CREATE DATABASE ${dbName};`)
+    await prisma.$queryRaw(Prisma.sql`CREATE IF NOT EXISTS DATABASE ${dbName};`)
   } catch (e) {
     // If database already exists, an error is thrown
     console.log(`Database ${dbName} already exists.`)
