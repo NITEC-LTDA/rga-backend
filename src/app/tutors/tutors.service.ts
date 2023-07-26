68eacc11f78e742331ef50d2f97051f24ed5a99c
Felipe Kafuri <felipe11.rk@gmail.com>
2023-07-25 23:49:02 -0300

    .

--- src/app/tutors/tutors.service.ts
+++ src/app/tutors/tutors.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { CreateTutorDto } from './dto/create-tutor.dto'
 import { UpdateTutorDto } from './dto/update-tutor.dto'
 import { Tutor } from './entities/tutor.entity'
