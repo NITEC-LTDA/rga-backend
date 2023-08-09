0ef96c56784f6fb57fb457b51db9a3beea924e9f
Felipe Kafuri <felipe11.rk@gmail.com>
2023-08-09 20:00:36 -0300

    pet entity

--- src/app/pets/entities/pet.entity.ts
+++ src/app/pets/entities/pet.entity.ts
@@ -1,1 +1,128 @@
-export class Pet {}
+import { randomUUID } from 'node:crypto'
+
+interface PetsProps {
+  id?: string
+  name: string
+  species: string
+  breed: string
+  birthDate: Date
+  gender: string
+  color: string
+  microchip: string
+  imageUrl: string
+  rga: string
+  tutorId: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+export class Pet {
+  private props: PetsProps
+  private _id: string
+
+  constructor(props: PetsProps, id?: string) {
+    this.props = {
+      ...props,
+      id: id ?? randomUUID(),
+    }
+  }
+
+  public get id() {
+    return this._id
+  }
+
+  public get name(): string {
+    return this.props.name
+  }
+
+  public set name(name: string) {
+    this.props.name = name
+  }
+
+  public get species() {
+    return this.props.species
+  }
+
+  public set species(species: string) {
+    this.props.species = species
+  }
+
+  public get breed() {
+    return this.props.breed
+  }
+
+  public set breed(breed: string) {
+    this.props.breed = breed
+  }
+
+  public get birthDate() {
+    return this.props.birthDate
+  }
+
+  public set birthDate(birdthdate: Date) {
+    this.props.birthDate = birdthdate
+  }
+
+  public get gender() {
+    return this.props.gender
+  }
+
+  public set gender(gender: string) {
+    this.props.gender = gender
+  }
+
+  public get color() {
+    return this.props.color
+  }
+
+  public set color(color: string) {
+    this.props.color = color
+  }
+
+  public get microchip() {
+    return this.props.microchip
+  }
+
+  public set microchip(microchip: string) {
+    this.props.microchip = microchip
+  }
+
+  public get imageUrl() {
+    return this.props.imageUrl
+  }
+
+  public set imageUrl(imageUrl: string) {
+    this.props.imageUrl = imageUrl
+  }
+
+  public get rga() {
+    return this.props.rga
+  }
+
+  public set rga(rga: string) {
+    this.props.rga = rga
+  }
+
+  public get tutorId() {
+    return this.props.tutorId
+  }
+
+  public set tutorId(tutorId: string) {
+    this.props.tutorId = tutorId
+  }
+
+  public get createdAt() {
+    return this.props.createdAt
+  }
+
+  public set createdAt(createdAt: Date) {
+    this.props.createdAt = createdAt
+  }
+
+  public get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  public set updatedAt(updatedAt: Date) {
+    this.props.updatedAt = updatedAt
+  }
+}
