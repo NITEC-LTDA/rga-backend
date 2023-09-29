c850cf69d23e8ba667bd606a1efcfd3ee43a8503
Felipe Kafuri <felipe11.rk@gmail.com>
2023-09-29 20:04:23 -0300

    pets and tutors reports

--- src/app/reports/reports.controller.ts
+++ src/app/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode } from '@nestjs/common'
+import { Controller, Get, HttpCode, Query } from '@nestjs/common'
 import { ReportsService } from './reports.service'
 
 @Controller('reports')
@@ -9,5 +9,30 @@ export class ReportsController {
   @HttpCode(200)
   countEntities() {
     return this.dashboardsService.countEntities()
+  }
+
+  @Get('/pets') // /reports/pets
+  @HttpCode(200)
+  petsReports(
+    // filters
+    @Query('neighborhood') neighborhood: string,
+    @Query('species') species: string,
+    @Query('breed') breed: string,
+  ) {
+    const filters = { neighborhood, species, breed }
+    return this.dashboardsService.petsReport(filters)
+  }
+
+  @Get('/tutors') // /reports/tutors
+  @HttpCode(200)
+  tutorsReports(
+    // filters
+    @Query('neighborhood') neighborhood: string,
+    @Query('city') city: string,
+    @Query('state') state: string,
+    @Query('zipcode') zipcode: string,
+  ) {
+    const filters = { neighborhood, state, city, zipcode }
+    return this.dashboardsService.tutorsReport(filters)
   }
 }
