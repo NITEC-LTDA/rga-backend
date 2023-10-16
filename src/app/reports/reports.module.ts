b04d8ac331b57b2f309e469622ccd4e882456e55
Felipe Kafuri <felipe11.rk@gmail.com>
2023-10-16 10:17:22 -0300

    fix reports module

--- src/app/reports/reports.module.ts
+++ src/app/reports/reports.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ReportsService } from './reports.service'
 import { ReportsController } from './reports.controller'
 import { DatabaseModule } from '@/infra/database/database.module'
+import { AdminsModule } from '../admins/admins.module'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AdminsModule],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
